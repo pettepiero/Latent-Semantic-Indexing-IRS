f1d9e94729007293fb3b43c6de079b30dc822a42
pettepiero <pette.piero@gmail.com>
2024-01-17 14:33:54 +0100

    Added some comments

--- lsi.py
+++ lsi.py
@@ -8,21 +8,14 @@ from numpy.linalg import norm
 from collections import Counter
 import string
 
-# import nltk
+# import Natural Language Toolkit for preprocessing the data
 import nltk
 
 nltk.download("wordnet")
 nltk.download("punkt")
 
-# preprocessing
-from nltk.corpus import stopwords  # stopwords
-from nltk import word_tokenize, sent_tokenize  # tokenizing
-from nltk.stem import (
-    PorterStemmer,
-    LancasterStemmer,
-)  # using the Porter Stemmer and Lancaster Stemmer and others
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem import WordNetLemmatizer  # lammatizer from WordNet
+from nltk import word_tokenize              # tokenizer
+from nltk.stem import WordNetLemmatizer     # lammatizer from WordNet
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -30,24 +23,29 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import Pipeline
 
+# Importing the data, preprocessing for TIME.ALL format
 
 with open("data/time/TIME.ALL", "r") as f:
     lines = f.read().split("*TEXT ")
     lines = lines[1:]
 
-documents_list = []
+documents_list = []     # list of tuples of strings (headline, content)
 
 for i, article in enumerate(lines):
     parts = article.split("\n\n", 1)
     documents_list.append((parts[0], parts[1].lstrip("\n").lower()))
 
-# Stop words
+# Importing stop words from TIME.STP dataset into a list
 with open("data/time/TIME.STP", "r") as f:
     sw = f.read().split("\n\n")
 stop_words = [word.lower() for word in sw]
+
+del lines
 del sw
 
-
+# Functions to clean the text: 1) tokenization, 2) lowercasing, 3) lemmatization
+# lowercasing is necessary because this function is used for the query as well (not in TIME.ALL format)
+# This preprocessing part uses some built-in functions from nltk. The lemmatizer is from WordNet
 def clean_text(headline):
     le = WordNetLemmatizer()
     word_tokens = word_tokenize(headline)
@@ -91,39 +89,19 @@ pipe = Pipeline(
     [("count", CountVectorizer(vocabulary=dictionary)), ("tfid", TfidfTransformer())]
 )
 tfidf = pipe.fit_transform(data["Cleaned content"])
-print(
-    f"Fitted pipeline on 'data['Cleaned content']' and obtained the following tfidf of shape {tfidf.shape}:"
-)
-print(tfidf.toarray())
+# print(
+#     f"Fitted pipeline on 'data['Cleaned content']' and obtained the following tfidf of shape {tfidf.shape}:"
+# )
+# print(tfidf.toarray())
 
-print("pipe['count'].transform(data['Cleaned content']).toarray()")
-print(pipe["count"].transform(data["Cleaned content"]).toarray())
-print(f"Also learnt the following idf of length {len(pipe['tfid'].idf_)}:")
-idf = pipe["tfid"].idf_
-print(idf)
-
-
-# vectorizer = CountVectorizer(vocabulary=dictionary)
-# X = vectorizer.fit_transform(data["Cleaned content"])
-# print(vectorizer.get_feature_names_out())
-# print("X:")
-# print(X.toarray())
-# print(X.shape)
-
-## Now do the tf-idf matrix
-# tf_transformer = TfidfTransformer()
-# doc_term = tf_transformer.fit_transform(X)
-# print(tf_transformer.get_feature_names_out())
-# print(tf_transformer.n_features_in_)
-# print("doc_term:")
-# print(doc_term.toarray())
-# idf = tf_transformer.idf_
-# print("idf:")
-# print(idf)
+# print("pipe['count'].transform(data['Cleaned content']).toarray()")
+# print(pipe["count"].transform(data["Cleaned content"]))
+# print(f"Also learnt the following idf of length {len(pipe['tfid'].idf_)}:")
+#print(idf)
+idf = pipe["tfid"].idf_ 
 
 dd = dict(zip(pipe.get_feature_names_out(), idf))
 sorted_dict = sorted(dd, key=dd.get)
-
 
 # Latent Semantic Analysis
 # documentation states that for LSA n_components should be 100
@@ -133,44 +111,42 @@ print("Latent Semantic Analysis")
 print("********************************************************\n")
 
 lsa_model = TruncatedSVD(
-    n_components=100, algorithm="randomized", n_iter=10, random_state=42
+    n_components=10, algorithm="randomized", n_iter=10, random_state=42
 )
 lsa_matrix = lsa_model.fit_transform(tfidf)
 
-print(f"lsa_matrix:\n {lsa_matrix}")
-print(f"lsa_matrix.shape: {lsa_matrix.shape}")
+print("Created latent semantic analysis model")
+# print(f"lsa_matrix:\n {lsa_matrix}")
+# print(f"lsa_matrix.shape: {lsa_matrix.shape}")
 
 # print(f"tf_transformer.feature_names_in: {tf_transformer.feature_names_in_}")
-print(f"tf_transformer.get_feature_names_out(): {pipe.get_feature_names_out()}")
+#print(f"tf_transformer.get_feature_names_out(): {pipe.get_feature_names_out()}")
 
 vocab = pipe.get_feature_names_out()
 
 for i, comp in enumerate(lsa_model.components_):
     vocab_comp = zip(vocab, comp)
     sorted_words = sorted(vocab_comp, key=lambda x: x[1], reverse=True)[:10]
-    print("Topic " + str(i) + ": ")
-    for t in sorted_words:
-        print(t[0], end=" ")
-    print("\n")
-
+    # print("Topic " + str(i) + ": ")
+    # for t in sorted_words:
+    #     print(t[0], end=" ")
+    # print("\n")
 
 def get_query():
     print("\n********************************************************")
-    # query_str = input("Write your free-text query: ")
-    query_str = "In a NumPy array, each row could represent a document, and columns could represent the index and similarity measure."
+    query_str = input("Write your free-text query: ")
+    # query_str = "In a NumPy array, each row could represent a document, and columns could represent the index and similarity measure."
     query = clean_text(query_str)
-    print(f"Your query is: {query}")
+    print(f"Nice, what I'm going to use is: {query}")
     return query
 
 
 def transform_query(query):
     query_vector = pipe.transform([query])
-    print(query_vector.toarray().shape)
+    print(f"query_vector: {query_vector}")
+    print(f"query_vector.shape: {query_vector.shape}")
     query_lsa = lsa_model.transform(query_vector).reshape(-1)
-    print(f"query_lsa = {query_lsa}")
-    print(f"query_lsa shape = {query_lsa.shape}")
     return query_lsa
-
 
 query = get_query()
 query_v = transform_query(query)
@@ -200,12 +176,19 @@ def ordered_measures(query_vector, docs_matr):
 
 
 results = sim_measures(query_v, lsa_matrix)
-# indices = list(range(len(results)))
-# res_idx = list(zip(indices, results))
 res_df = pd.DataFrame(data=results)
-#res_df = pd.DataFrame(data=res_idx)
 res_df.columns = ["Similarity measure"]
-# del res_idx
 res_df = res_df.sort_values(by="Similarity measure", ascending=False)
-print(f"\n\ndataframe: {res_df}")
 
+
+def print_top_results(res_df, n_results=5):
+    print(f"\n\nTop 3 results:")
+    for i in range(3):
+        print(f"{i+1}. {res_df.iloc[i, 0]}")
+    print(f"\n\n{res_df.iloc[:n_results]}")
+    top3_doc_indices = res_df.iloc[:n_results, 0].index.tolist()
+    for doc in top3_doc_indices:
+        print(f"\n\n{documents_list[doc][0]}")
+        print(f"{documents_list[doc][1]}")
+
+print_top_results(res_df, 3)
